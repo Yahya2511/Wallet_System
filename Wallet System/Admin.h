555178c5f8a9cb5e5d7f1d9db61eaa10cd07321f
Mohamed Gaber <137436028+muhammaddgaberr@users.noreply.github.com>
2024-05-04 23:27:55 +0300

    Update Admin.h

--- Wallet System/Admin.h
+++ Wallet System/Admin.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 #include <map>
 
 using namespace std;
@@ -17,10 +18,12 @@ public:
     bool static login(string, string);
 
     void AddUser(map<string, class User>& usersMap, double initialBalance);
-    void EditUsersPassword(map<string, User>& users, string userName);
+    void EditUsersPassword(map<string, class User>& users, string userName);
     void DeleteUser(map<string, class User>& usersMap, string userName);
     void SuspendUser(map<string, class User>& usersMap, string userName);
     void ReactivateUser(map<string, class User>& usersMap, string userName);
+
+    void ViewUniTransaction(stack<class Transaction> sysHistory);
   
     void viewUsersInfo(map<string, User>& users);
 };
